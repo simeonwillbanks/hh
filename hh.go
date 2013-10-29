ab5eb188abfb98fa747a5934b4727c71f9ff170a
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2013-10-28 21:21:24 -0700

    Add format script and format hh.go

--- hh.go
+++ hh.go
@@ -4,13 +4,13 @@ import "os"
 import "github.com/codegangsta/cli"
 
 func main() {
-  app := cli.NewApp()
-  app.Name = "hh"
-  app.Usage = "Hacking + Health = Happiness"
-  app.Version = "0.0.1"
-  app.Action = func(c *cli.Context) {
-    println("Hello Healthy Hacker!")
-  }
+	app := cli.NewApp()
+	app.Name = "hh"
+	app.Usage = "Hacking + Health = Happiness"
+	app.Version = "0.0.1"
+	app.Action = func(c *cli.Context) {
+		println("Hello Healthy Hacker!")
+	}
 
-  app.Run(os.Args)
+	app.Run(os.Args)
 }
